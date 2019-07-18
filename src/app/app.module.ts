5777619ad3270e6dd576ad2cc6558107f5f83a8d
jonathan wolfe <jonathanwolfe@jonathans-MacBook-Pro.local>
2019-07-18 14:48:14 -0500

    added componets, first push test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MySettingsComponent } from './my-settings/my-settings.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MySettingsComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
